c17c6f69f9a92ef9dfaa7dbb6bce45c766444a5b
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-05-15 11:52:20 +0300

    fixed server

--- src/server/server.js
+++ src/server/server.js
@@ -9,13 +9,13 @@ app.set('port', process.env.PORT || 4000);
 
 app.engine('hbs', hbs ({
   defaultLayout: 'index',
-  layoutDir: path.join(__dirname, '..', 'views','layout'),
-  partialsDir: path.join(__dirname, '..', 'views', 'partials'),
+  layoutDir: path.join(__dirname,'..','/views/layouts'),
+  partialsDir: path.join(__dirname, '..', 'views/partials'),
   extname: 'hbs'
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, '..','..', 'views'));
 
 app.use(router);
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
