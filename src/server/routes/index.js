90680db98b1d6d004f710f068aba020fabed59f1
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-05-16 14:50:57 +0300

    create new tomorrow page, change event page to event today

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const homePage = require('./home.js');
+const eventTodayPage = require('./event-today.js');
+const eventTomorrowPage = require('./event-tomorrow.js');
 
 router.get('/', homePage);
+router.get('/event-today', eventTodayPage);
+router.get('/event-tomorrow', eventTomorrowPage);
 
 module.exports = router
