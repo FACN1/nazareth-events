62d49e3ce1b7b7fce9456cb5bb11e12172e3cdd4
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-05-17 13:19:49 +0300

    page for map

--- public/main-map.js
+++ public/main-map.js
@@ -1,10 +1,4 @@
 // globals L
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
-}).addTo(mymap);
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibW9oYW1lZG9tYXJpaSIsImEiOiJjajJzdGVlcTQwMDU4MzNzM2d5emkxcmhwIn0.9dMJv1kQOZovU2TVvZ5FIw').addTo(mymap);
