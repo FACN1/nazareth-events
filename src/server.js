61bf54d29b35689503ffd1cb0454c8d94d2d4ee4
hilbertspitzer <hilbertbonamy@gmail.com>
2017-05-22 17:54:36 +0300

    has a handlebar

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
-const express = require('express');
-var server = express();
+var express = require('express')
+var server = express() // remember this!
+var hbs = require('express-handlebars')
+var path = require('path');
 
-server.get('/', function(req, res){
-  res.json({message: 'this is a useless object, but whatever'})
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+
+server.get('/', function (req, res) {
+  res.render('home')
 })
 
-module.exports = server;
+module.exports = server
+
+
+// const express = require('express');
+// var server = express();
+//
+// server.get('/', function(req, res){
+//   res.json({message: 'this is a useless object, but whatever'})
+// })
+//
+// module.exports = server;
