db80ab9d53a871aaf6f61652e4f5fafa742b0599
Matt Lubel <matt_lubel@hotmail.com>
2017-05-24 14:14:29 +0300

    #57 start authenticate/ route

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ var express = require('express')
 var server = express()
 var hbs = require('express-handlebars')
 var path = require('path')
+const bodyParser = require('body-parser')
 
 server.use(express.static(path.join(__dirname, '../public')))
+server.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 server.engine('hbs', hbs({
   defaultLayout: 'main',
@@ -19,5 +23,25 @@ server.get('/', function (req, res) {
 server.get('/organisations/login', (req, res) => {
   res.render('organisations_login')
 })
+
+// server.post('/authenticate', (req, res) => {
+//   const { username, password } = req.body
+//   db.getOrganizationByUsername(username, (err, organization) => {
+//     if (err) {
+//       return res.send('db error :(')
+//     }
+//     if (organization === null) {
+//       return res.render('organisations_login', {
+//         errorMessage: 'username not recognised.'
+//       })
+//     } else if (password !== user.password) {
+//       return res.render('organisations_login', {
+//         errorMessage: 'incorrect password.'
+//       })
+//     }
+//     // generate token
+//     // send token
+//   })
+// })
 
 module.exports = server
