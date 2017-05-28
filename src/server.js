0a527a677f17ab9e506dc673351f9f5be11ef345
hilbertspitzer <hilbertbonamy@gmail.com>
2017-05-28 11:20:22 +0300

    #50 creates a list page ordered by dates

--- src/server.js
+++ src/server.js
@@ -13,7 +13,55 @@ server.engine('hbs', hbs({
 server.set('view engine', 'hbs')
 
 server.get('/', function (req, res) {
-  res.render('home')
+  res.render('home', {
+    datesWithEvents: [
+      {
+        date: 'Wednesday, 24 May',
+        events: [
+          {
+            title: 'Pangea reunification',
+            description: 'Australia joined Europe in Eurovision, now its time for something MORE!',
+            time: '23:59',
+            location: 'THE WORLD!!!'
+          },
+          {
+            title: '@_@ Swirly glasses stories',
+            description: 'WE all read books, but heres a guy who really can show off what he has learned',
+            time: 'summer equinox',
+            location: 'ON TOP OF A MOUNTAIN!!!'
+          }
+        ]
+      },
+      {
+        date: 'Thursday, 25 May',
+        events: [
+          {
+            title: 'Pangea destruction',
+            description: 'Australia joined Europe in Eurovision, now its time for something MORE!',
+            time: '00:00',
+            location: 'LIWAN!!!'
+          },
+          {
+            title: 'training on thorns',
+            description: 'a traditional African festival in which strong fighters dance on thorns',
+            time: '20:00-21:00',
+            location: 'South-African-American African-American African Center'
+          }
+        ]
+      },
+      {
+        date: 'Friday, 26 May',
+        events: [
+          {
+            title: 'Khan El Basha history lessons',
+            description: 'Hitsory lessons can be fun too...if you bring chocolates',
+            time: '15:00-19:00',
+            location: 'Khan El Basha'
+          }
+        ]
+      }
+    ]
+  })
 })
 
 module.exports = server
