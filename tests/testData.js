6924e6b7ec37edf767bd3bdf51a31c7c2581fee1
Matt Lubel <matt_lubel@hotmail.com>
2017-05-30 10:36:46 +0300

    #66 add one more test

--- tests/testData.js
+++ tests/testData.js
@@ -1,37 +1,61 @@
 module.exports = {
 
   formatEvents: [
+    // test 1
     {
       input: [
-        {title: 'test me baby', date: '2011-03-03'}
+        {title: 'test me', date: '2011-03-03'}
       ],
       expectedOutput: [
         {
           date: '2011-03-03',
           events: [
-            {title: 'test me baby', date: '2011-03-03'}
+            {title: 'test me', date: '2011-03-03'}
           ]
         }
       ],
-      message: 'should return correct result for list of one event'
+      message: 'should return correct result for list of one event.'
     },
+    // test 2
     {
       input: [
-        {title: 'test me baby', date: '2011-03-03'},
-        {title: 'test me too baby', date: '2011-03-03'},
-        {title: 'test me again baby', date: '2011-03-03'}
+        {title: 'test me', date: '2011-03-03'},
+        {title: 'test me too', date: '2011-03-03'},
+        {title: 'test me again', date: '2011-03-03'}
       ],
       expectedOutput: [
         {
           date: '2011-03-03',
           events: [
-            {title: 'test me baby', date: '2011-03-03'},
-            {title: 'test me too baby', date: '2011-03-03'},
-            {title: 'test me again baby', date: '2011-03-03'}
+            {title: 'test me', date: '2011-03-03'},
+            {title: 'test me too', date: '2011-03-03'},
+            {title: 'test me again', date: '2011-03-03'}
           ]
         }
       ],
-      message: 'should return correct result for 3 events on one date'
+      message: 'should return correct result for 3 events on one date.'
+    },
+    // test 3
+    {
+      input: [
+        {title: 'test me', date: '2011-03-03'},
+        {title: 'test me again', date: '2011-04-03'}
+      ],
+      expectedOutput: [
+        {
+          date: '2011-03-03',
+          events: [
+            {title: 'test me', date: '2011-03-03'}
+          ]
+        },
+        {
+          date: '2011-04-03',
+          events: [
+            {title: 'test me again', date: '2011-04-03'}
+          ]
+        }
+      ],
+      message: 'should return correct result for 2 events on consecutive dates.'
     }
   ]
 
